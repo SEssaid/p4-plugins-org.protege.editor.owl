b4c61daa1e822c05ffa9d51095a5d0d80695470f
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-12-19 01:20:45 +0000

    Improved some mildly questionable code involving the ontology id  and physical location.
    git-svn-id: http://smi-protege.stanford.edu/repos/protege/protege4/plugins/org.protege.editor.owl/branches/xmlcatalog@16507 30a32298-b1ee-0310-87e3-a7336bc3aa68

--- src/org/protege/editor/owl/model/OWLModelManagerImpl.java
+++ src/org/protege/editor/owl/model/OWLModelManagerImpl.java
@@ -314,8 +314,15 @@ public class OWLModelManagerImpl extends AbstractModelManager
                                                    ".\nPlease open the ontology in a new frame.");
         }
         else{
+        	
             // Set up a mapping from the ontology URI to the physical URI
-            manager.addIRIMapper(new SimpleIRIMapper(id.getOntologyIRI(), uri));
+        	if (id.getVersionIRI() != null) {
+        		manager.addIRIMapper(new SimpleIRIMapper(id.getVersionIRI(), uri));
+        	}
+        	else {
+        		manager.addIRIMapper(new SimpleIRIMapper(id.getOntologyIRI(), uri));
+        	}
+            
             if (uri.getScheme()  != null && uri.getScheme().equals("file")) {
                 // Load the URIs of other ontologies that are contained in the same folder.
                 addRootFolder(uri);
